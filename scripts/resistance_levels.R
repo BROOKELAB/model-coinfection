c055e54dfd9fd80baf00c2222e183f2fda92355c
Mireille Farjo <mnfarjo@gmail.com>
2025-07-20 08:55:00 -0500

    scripts/

--- scripts/resistance_levels.R
+++ scripts/resistance_levels.R
@@ -74,7 +74,7 @@ calc.viable.cor(viables.35.c) # r = -0.07171804 #p = 0.109219
 
 
 #calculate fitness for each genotype
-load("rdata/resistance_fitness_list.RData")
+load("rdata/resistance_gen_list.RData")
 load("rdata/combo.RData")
 
 calc.fitness.noepi <- function(gen){
